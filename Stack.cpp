5d2b8b9701edbdf089cd65664426a421cc65c033
gs0430 <113073281+gs0430@users.noreply.github.com>
2024-06-10 21:48:47 -0400

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,10 +1,3 @@
-// $Id: Stack.cpp 827 2011-02-07 14:20:53Z hillj $
-
-// Honor Pledge:
-//
-// I pledge that I have neither given nor received any help
-// on this assignment.
-
 //Stack default constructor
 #include <iostream>
 template <typename T>
